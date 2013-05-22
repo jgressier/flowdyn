62e9f82a0b080baccbcb63e70bff7bda2d890b1e
J. Gressier <jeremie.gressier@isae.fr>
2013-05-22 22:39:34 +0000

    pyfvm: general rk class with rk3ssp and rk2 implementation

--- integration.py
+++ integration.py
@@ -52,6 +52,27 @@ class time_explicit(timemodel):
         field.add_res(dtloc)
         return field
     
+class rkmodel(timemodel):
+    def step(self, field, dtloc, butcher):
+        #butcher = [ np.array([1.]), \
+        #            np.array([0.25, 0.25]), \
+        #            np.array([1., 1., 4.])/6. ]
+        prhs = []
+        pfield = numfield(field)            
+        for pcoef in butcher:
+            # compute residual of previous stage and memorize it in prhs[]
+            prhs.append([ q.copy() for q in self.calcrhs(pfield)])
+            # revert to initial step
+            pfield.qdata = [ q.copy() for q in field.qdata ]
+            # aggregate residuals
+            for qf in pfield.residual:
+                qf *= pcoef[-1]
+            for i in range(pcoef.size-1):
+                for q in range(pfield.neq):
+                    pfield.residual[q] += pcoef[i]*prhs[i][q]
+            pfield.add_res(dtloc)        
+        return pfield
+
 class time_rk2(timemodel):
     def step(self, field, dtloc):
         reffield = numfield(field)
@@ -61,3 +82,20 @@ class time_rk2(timemodel):
         reffield.residual = field.residual
         reffield.add_res(dtloc)
         return reffield
+
+class time_rk3ssp(rkmodel):
+    def step(self, field, dtloc):
+        butcher = [ np.array([1.]), \
+                    np.array([0.25, 0.25]), \
+                    np.array([1., 1., 4.])/6. ]
+        return rkmodel.step(self, field, dtloc, butcher)
+
+class time_rk4(rkmodel):
+    def step(self, field, dtloc):
+        butcher = [ np.array([0.5]), \
+                    np.array([0., 0.5]), \
+                    np.array([0., 0., 1.]), \
+                    np.array([1., 2., 2., 1.])/6. ]
+        return rkmodel.step(self, field, dtloc, butcher)
+
+
--- test.py
+++ test.py
@@ -18,7 +18,7 @@ from integration import *
 
 mesh1 = unimesh(ncell=100, length=1.)
 
-endtime = 20
+endtime = 5
 ntime   = 1
 tsave   = linspace(0, endtime, num=ntime+1)
 
@@ -26,10 +26,10 @@ cfl=0.5
 conv     = convmodel(1.)
 xmeth1   = extrapol1()
 xmeth2   = extrapol2()
-xmethk   = extrapol3()
-expl_o1 = time_rk2(mesh1, xmeth1)
-expl_o2 = time_rk2(mesh1, xmeth2)
-expl_o3  = time_rk2(mesh1, xmethk)
+xmeth3   = extrapol3()
+expl_o1 = time_rk3ssp(mesh1, xmeth1)
+expl_o2 = time_rk3ssp(mesh1, xmeth2)
+expl_o3 = time_rk3ssp(mesh1, xmeth3)
 
 # TODO : make init method for scafield 
 # nombre d'onde
@@ -37,11 +37,11 @@ k = 1
 field0 = scafield(conv, mesh1.ncell)
 field0.qdata[0] = sin(2*k*pi/mesh1.length*mesh1.centers())   
 #field0.qdata[0] = (1+sign(-(mesh1.centers()/mesh1.length-.25)*(mesh1.centers()/mesh1.length-.75)))/2
-#field0.qdata[0] = sin(2*2*pi/mesh1.length*mesh1.centers())*(1+sign(-(mesh1.centers()/mesh1.length-.25)*(mesh1.centers()/mesh1.length-.75)))/2 
+field0.qdata[0] = sin(2*2*pi/mesh1.length*mesh1.centers())*(1+sign(-(mesh1.centers()/mesh1.length-.25)*(mesh1.centers()/mesh1.length-.75)))/2 
 
 results1 = expl_o1.solve(field0, cfl, tsave)
 results2 = expl_o2.solve(field0, cfl, tsave)
-resultsk = expl_k.solve(field0, cfl, tsave)
+results3 = expl_o3.solve(field0, cfl, tsave)
 
 print expl_o1.nit," iterations"
 
@@ -53,9 +53,9 @@ labels.append("initial condition")
 for i in range(1,len(results1)):
     plot(mesh1.centers(), results1[i].qdata[0], style[i])
     plot(mesh1.centers(), results2[i].qdata[0], style[i])
-    plot(mesh1.centers(), resultsk[i].qdata[0], style[i])
+    plot(mesh1.centers(), results3[i].qdata[0], style[i])
     labels.append("O1, t=%.1f"%results1[i].time)
     labels.append("O2, t=%.1f"%results2[i].time)
-    labels.append("O3, t=%.1f"%results2[i].time)
+    labels.append("O3, t=%.1f"%results3[i].time)
 legend(labels, loc='upper left',prop={'size':10})  
 show()
