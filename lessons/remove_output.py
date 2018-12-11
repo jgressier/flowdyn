5178234f6911d4681590f018ef895f3812bfaebc
j.gressier <jeremie.gressier@isae.fr>
2018-12-11 10:51:30 +0100

    reorganize and commit cleaned python notebooks ; add euler/sod notebook

---
+++ lessons/remove_output.py
@@ -0,0 +1,29 @@
+"""
+Usage: python remove_output.py notebook.ipynb [ > without_output.ipynb ]
+Modified from remove_output by Minrk
+
+"""
+import sys
+import io
+import os
+from nbformat.current import read, write
+
+
+def remove_outputs(nb):
+    """remove the outputs from a notebook"""
+    for ws in nb.worksheets:
+        for cell in ws.cells:
+            if cell.cell_type == 'code':
+                cell.outputs = []
+
+if __name__ == '__main__':
+    for fname in sys.argv[1:]:
+        bakname = fname+'.bak'
+        os.rename(fname, bakname)
+        with io.open(bakname, 'r') as f:
+            nb = read(f, 'json')
+        remove_outputs(nb)
+        #base, ext = os.path.splitext(fname)
+        with io.open(fname, 'w', encoding='utf8') as f:
+            write(nb, f, 'json', version=4)
+        print "wrote cleaned %s and backward copy %s" % (fname, bakname)
