544e3c6af9296319f991ea66fba8bf2107176f57
j.gressier <jeremie.gressier@isae.fr>
2019-04-08 17:22:23 +0200

    add functions to get bc and var list from model

--- pyfvm/modelphy/base.py
+++ pyfvm/modelphy/base.py
@@ -44,11 +44,18 @@ class model():
         self.has_firstorder_terms  = 0
         self.has_secondorder_terms = 0
         self.has_source_terms      = 0
-        self._bcdict = { 'dirichlet': self.bc_dirichlet }
+        self._vardict = { }
+        self._bcdict  = { 'dirichlet': self.bc_dirichlet }
 
     def __repr__(self):
         print "model: ", self.equation
         print "nb eq: ", self.neq
+
+    def list_bc(self):
+        return self._bcdict.keys()
+
+    def list_var(self):
+        return self._vardict.keys()
         
     def cons2prim(self):
         print "cons2prim method not implemented"
